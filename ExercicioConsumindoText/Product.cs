586625d00d1e5b18270d4fd4f9de8fe61d121ffb
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-29 16:55:48 -0300

    Add project files.

---
+++ ExercicioConsumindoText/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercicioConsumindoText
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+    }
+}
---
+++ ExercicioConsumindoText/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExercicioConsumindoText;
+using System.Globalization;
+
+Console.Write("Enter full file path: ");
+string path = Console.ReadLine();
+
+if (path is null)
+    return;
+
+using StreamReader file = new StreamReader(path);
+string? line;
+List<Product> products = new List<Product>();
+
+while ((line = file.ReadLine()) is not null)
+{ 
+    string[] text = line.Split(',');
+    string name = text[0];
+    double price = double.Parse(text[1], CultureInfo.InvariantCulture);
+
+    Product product = new Product(name, price);
+    products.Add(product);
+}
+
+Console.WriteLine($"Average price: {products.Average(x => x.Price).ToString("F2")}"); 
+products.OrderByDescending(x => x.Name).ToList().ForEach(x =>  Console.WriteLine(x.Name));
+
+file.Close();
+
+Console.ReadKey();
